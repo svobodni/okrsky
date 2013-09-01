cc9cf97f27b78a893c164e28d1d08e41b239182c
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2013-09-01 09:15:29 +0200

    Generated Rails 4 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Okrsky::Application.config.secret_key_base = '557a1c997047077367ccef0459dc5c17397aebc0fbf3b4c4c9ad26ff4c826a2f327805993de962ce0b282177f68da21f4ef7e5933bf0ea343f20b6db1567b50f'
