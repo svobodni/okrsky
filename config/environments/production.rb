3acb3de5d2e9c79ef4450902761046068a5d414b
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2013-09-05 18:08:25 +0200

    Mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,16 @@ Okrsky::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'mail.dotnethosting.cz',
+    port:                 465,
+    domain:               'svobodni.cz',
+    user_name:            'kubicek@svobodni.cz',
+    password:             'sean78:cams',
+    authentication:       'login',
+    ssl: true
+  }
+
 end
