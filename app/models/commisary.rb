8b00b960e87e053b3f9c571c58fc57dff2110e9b
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2014-03-06 13:36:29 +0100

    Oprava validace delky data narozeni

--- app/models/commisary.rb
+++ app/models/commisary.rb
@@ -4,7 +4,7 @@ class Commisary < ActiveRecord::Base
   belongs_to :town_hall
 
   validates :name, length: { minimum: 5 }
-  validates :birth_number, length: { minimum: 9 }
+  validates :birth_number, length: { minimum: 8 }
   validates :address, length: { minimum: 12 }
   validates :email, presence: true, length: { minimum: 5 }
   validates :ward, presence: true, uniqueness: true
