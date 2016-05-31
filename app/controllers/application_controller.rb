a59f0acabc65cc26ae507b186f980df4c6221908
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2016-05-31 02:41:52 +0200

    Oprava opravneni clenu komisi

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,4 +29,7 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, :alert => exception.message
   end
 
+  def current_ability
+    @current_ability ||= Ability.new(current_person)
+  end
 end
