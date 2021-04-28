aa255b6146bdea5724c26adc79b1e94f05c2e926
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2021-04-29 00:17:36 +0200

    Novy deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server "s3.svobodni.cz", port: 2204, user: "okrsky.svobodni", roles: %w{web app db}
+server "s3.svobodni.cz", port: 2207, user: "okrsky.svobodni", roles: %w{web app db}
 set :deploy_to, '~/app'
