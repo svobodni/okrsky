b4a499527c250d0686b2399b9a537b7113cf49a0
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2021-04-28 23:55:07 +0200

    Rails 5.2.5

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,6 @@ module Okrsky
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -22,5 +20,8 @@ module Okrsky
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :cs
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
